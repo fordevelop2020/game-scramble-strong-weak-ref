e3433479113068fc9db6f6ee661f1a966b51e90c
fordevelop2020 <fordevelop2020@gmail.com>
2021-12-10 17:39:40 +0100

    showErrorMethode

--- Project05/ViewController.swift
+++ Project05/ViewController.swift
@@ -11,10 +11,13 @@ class ViewController: UITableViewController {
     var allWords = [String]()
     var usedWords = [String]()
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
+        
         
         if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
             if let startWord = try? String(contentsOf: startWordsUrl) {
@@ -29,7 +32,7 @@ class ViewController: UITableViewController {
       
     }
     
-    func startGame(){
+   @objc func startGame(){
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -65,34 +68,62 @@ class ViewController: UITableViewController {
         
         let lowerCaseAnswer = answer.lowercased()
         let errorTitle : String
-        let errorMessage: String
+        let errorMessage : String
         
-        if isPossible(word: lowerCaseAnswer){
+        
+        if isSame(word: lowerCaseAnswer) {
+          if isPossible(word: lowerCaseAnswer){
             if isOriginal(word: lowerCaseAnswer){
                 if isReal(word: lowerCaseAnswer){
-                    usedWords.insert(answer, at: 0)
+                    usedWords.insert(answer.lowercased(), at: 0)
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
                     
                     return
                     
                 } else {
-                    errorTitle = "Word not recognized!"
+                   
+                    errorTitle = "Word not recognized! or shorter"
                     errorMessage = "You can't just make them up !"
+                    showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
                 }
             } else {
-                errorTitle = "Word already used!"
-                errorMessage = "Be more original!"
+               
+                   errorTitle = "Word already used!"
+                   errorMessage = "Be more original!"
+                showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
             }
         } else {
+           
             guard let title = title else { return }
             errorTitle = "Word not possible!"
             errorMessage = "You can't spell that word from \(title.lowercased())!"
+             showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
         }
-        
+        } else {
+           
+            guard let title = title else { return }
+            errorTitle = "Word is the same to \(title.lowercased())"
+            errorMessage = "Change it please"
+            showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
+        }
+
+    }
+    
+    func showErrorMessage(errorTitle : String , errorMessage: String){
         let av = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-        av.addAction(UIAlertAction(title: "OK", style: .default))
-        present(av, animated: true)
+               av.addAction(UIAlertAction(title: "OK", style: .default))
+               present(av, animated: true)
+    
+    }
+    
+    
+    
+    func isSame(word: String) -> Bool{
+        if title?.lowercased() == word {
+            return false
+        }
+        return true
     }
     
     func isReal(word: String) -> Bool {
@@ -100,8 +131,17 @@ class ViewController: UITableViewController {
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspeeledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         
+        if (misspeeledRange.location == NSNotFound) && (word.utf16.count >= 3){
+            return true
+        } else if (misspeeledRange.location != NSNotFound) || (word.utf16.count < 3) {
+            return false
+        }
+//        else if (misspeeledRange.location == NSNotFound) && (word.utf16.count < 3) {
+//            return false
+//        }
+        return false
         
-        return misspeeledRange.location == NSNotFound
+        
     }
     func isOriginal(word: String) -> Bool {
         return !usedWords.contains(word)
